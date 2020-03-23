a40ca8edea3792eebdd808f4b7a4189d4cb27cc9
wangluting <m13051699286@163.com>
2020-03-24 01:13:49 +0800

    --添加新功能： 网页刷新神器

---
+++ OnlineShoppingEdge/Modules/OtherTool/WebRefreshController.h
@@ -0,0 +1,17 @@
+//
+//  WebRefreshController.h
+//  OnlineShoppingEdge
+//
+//  Created by luting on 20/3/24.
+//  Copyright © 2020 luting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WebRefreshController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
